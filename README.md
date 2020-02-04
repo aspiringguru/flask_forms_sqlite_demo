3c9df53e057d19750271240c7eca44e65a984a6b
aspiringguru <bmatthewtaylor@gmail.com>
2020-02-05 09:14:28 +1000

    basic form and sql create read demo

--- README.md
+++ README.md
@@ -14,3 +14,25 @@ source env/bin/activate
 (env)$ pip freeze
 
 pip install flask-wtf
+
+
+--------------------------notes ------------------------------------------------
+If you want to retrieve POST data,
+
+first_name = request.form.get("firstname")
+- If you want to retrieve GET (query string) data,
+
+first_name = request.args.get("firstname")
+- Or if you don't care/know whether the value is in the query string or in the post data,
+
+first_name = request.values.get("firstname").
+- request.values is a CombinedMultiDict that combines Dicts from request.form and request.args.
+--------------------------notes ------------------------------------------------
+
+
+
+https://www.tutorialspoint.com/flask/flask_sqlite.htm
+
+
+
+sudo apt install sqlite3
--- app_forms.py
+++ app_forms.py
@@ -1,9 +1,58 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
+import sqlite3 as sql
+
 
 
 app = Flask(__name__)
 app.secret_key = 'development key'
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
+
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   print("@app.route('/addrec'")
+   if request.method == 'POST':
+      print("request.method == 'POST'")
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) \
+               VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            con.commit()
+            msg = "Record successfully added"
+            print("Record successfully added")
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+         print("error in insert operation")
+      finally:
+         con.close()
+         return render_template("result.html",msg = msg)
+   else:
+      print("request.method != 'POST'")
+      return "request.method != 'POST'"
+
+
+@app.route('/list')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   cur = con.cursor()
+   cur.execute("select * from students")
+   rows = cur.fetchall();
+   return render_template("list.html",rows = rows)
 
 
 @app.route('/contact', methods = ['GET', 'POST'])
